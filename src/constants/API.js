94e55c296634e97665af5e08c2a6fe60fa4590df
unknown <rinas.nkt@gmail.com>
2023-12-10 12:46:23 +0530

    lastone

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,2 +1,6 @@
-export const guestAPI = "http://localhost:5000/";
-export const adminAPI = "http://localhost:5000/admin";
+// export const guestAPI = "http://localhost:5000/";
+// export const adminAPI = "http://localhost:5000/admin";
+
+
+export const guestAPI = "https://shoemee.shop/";
+export const adminAPI = "https://shoemee.shop/admin";
